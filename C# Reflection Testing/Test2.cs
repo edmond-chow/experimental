552999fc0f4c1c565ae7febc916fc2dd2cf97a0f
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2024-04-30 00:43:29 +0800

    Add files via upload

---
+++ C# Reflection Testing/Test2.cs
@@ -0,0 +1,24 @@
+        static void Main(string[] args)
+        {
+            int i = 1234;
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(int.TryParse(i.ToString(), out i));
+
+
+
+            Type string_t = AppDomain.CurrentDomain.GetAssemblies()[0].GetType("System.String");     // typeof(string)
+            Type int_t = AppDomain.CurrentDomain.GetAssemblies()[0].GetType("System.Int32");         // typeof(int)
+            Type bool_t = AppDomain.CurrentDomain.GetAssemblies()[0].GetType("System.Boolean");      // typeof(bool)
+            Type console_t = AppDomain.CurrentDomain.GetAssemblies()[0].GetType("System.Console");   // typeof(System.Console)
+
+            object i_dyn = 1234;
+            console_t.GetMethod("WriteLine", new Type[] { string_t }).Invoke(null, new object[] {
+                int_t.GetMethod("ToString", new Type[] { }).Invoke(i_dyn, new object[] { })
+            });
+            console_t.GetMethod("WriteLine", new Type[] { bool_t }).Invoke(null, new object[] {
+                int_t.GetMethod("TryParse", new Type[] { string_t, int_t.MakeByRefType() }).Invoke(null, new object[] {
+                    int_t.GetMethod("ToString", new Type[] { }).Invoke(i_dyn, new object[] { }),
+                    i_dyn
+                })
+            });
+        }

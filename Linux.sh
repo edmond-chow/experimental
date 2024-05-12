8f160f933b9452598d1d1b611f1184e589ae00cf
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2024-05-12 17:33:23 +0800

    Create Linux.sh

---
+++ Linux.sh
@@ -0,0 +1,11 @@
+sudo apt install net-tools
+sudo apt install xterm
+sudo apt install xtitle
+sudo apt install build-essential dkms linux-headers-$(uname -r)
+sudo apt install openssh-server
+sudo apt install libx11-dev
+sudo apt-get install libsfml-dev
+Vbox_Gas_6.1.36/autorun.sh
+sudo cp lib.conf /etc/ld.so.conf.d
+sudo gedit ld.so.conf
+ldconfig

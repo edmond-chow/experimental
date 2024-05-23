cba2474668acd608ef346b00f502ec50711c888e
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2024-05-23 18:31:09 +0800

    Add files via upload

---
+++ js Loading WASM/Loading WASM.js
@@ -0,0 +1,26 @@
+    /* { wasm } */
+    let wasmExports = null;
+    let wasmMemory = new WebAssembly.Memory({ initial: 256, maximum: 256 });
+    let wasmTable = new WebAssembly.Table({ initial: 1, maximum: 1, element: 'anyfunc' });
+    let asmLibraryArg = {
+        '__handle_stack_overflow': () => {},
+        'emscripten_resize_heap': () => {},
+        '__lock': () => {},
+        '__unlock': () => {},
+        'memory': wasmMemory,
+        'table': wasmTable,
+        '_log': console.log,
+        '__cxa_throw': () => {},
+        'emscripten_run_script': () => {},
+        '__cxa_rethrow': () => {}
+    }
+    let info = {
+        'env': asmLibraryArg,
+        'wasi_snapshot_preview1': asmLibraryArg
+    };
+    (async () => {
+        let response = await fetch('Complex Testing Console.wasm');
+        let bytes = await response.arrayBuffer();
+        let wasmObj = await WebAssembly.instantiate(bytes, info);
+        wasmExports = wasmObj.instance.exports;
+    })();

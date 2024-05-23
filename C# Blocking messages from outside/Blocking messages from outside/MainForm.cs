a4a91a9e01706e8292a520bc6a5553c1246effe7
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2024-05-23 19:56:46 +0800

    Add files via upload

---
+++ C# Blocking messages from outside/Blocking messages from outside/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace Blocking_messages_from_outside
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        [DllImport("User32.dll")]
+        private extern static bool InSendMessage();
+        protected override void WndProc(ref Message m)
+        {
+            if (InSendMessage()) { return; }
+            base.WndProc(ref m);
+        }
+    }
+}

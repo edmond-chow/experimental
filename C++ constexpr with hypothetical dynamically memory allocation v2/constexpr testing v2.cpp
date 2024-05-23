d498c6f8a1fc6da22eec6963b007327c3b7ef193
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2024-05-23 18:51:38 +0800

    Add files via upload

---
+++ C++ constexpr with hypothetical dynamically memory allocation v2/constexpr testing v2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+struct MyStruct
+{
+private:
+    std::uint8_t* ptr;
+    size_t s_t;
+public:
+    constexpr MyStruct(const size_t s_t) : ptr(new std::uint8_t[s_t]{}), s_t(s_t) { std::fill_n(ptr, s_t, 0xCC); };
+    constexpr ~MyStruct() { delete[] ptr; };
+    constexpr const std::uint8_t* data() const { return ptr; }
+    constexpr const size_t size() const { return s_t; }
+};
+constexpr size_t f()
+{
+    return MyStruct(100).size();
+};
+constexpr const std::uint8_t g(const ptrdiff_t diff_t)
+{
+    return MyStruct(100).data()[diff_t];
+};
+int main()
+{
+    constexpr std::uint8_t s = g(99);
+    std::cout << (size_t)s << std::endl;
+    std::string line;
+    std::getline(std::cin, line);
+};

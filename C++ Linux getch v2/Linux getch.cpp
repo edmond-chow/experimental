3fbee81d84eb09d68dbc9b689845ee325f2b24b7
Edmond Chow <45330596+edmond-chow@users.noreply.github.com>
2024-06-07 13:57:43 +0800

    Update Linux getch.cpp

--- C++ Linux getch v2/Linux getch.cpp
+++ C++ Linux getch v2/Linux getch.cpp
@@ -12,7 +12,7 @@ char getch()
     current.c_cc[VTIME] = 0;
     current.c_cc[VMIN] = 1;
     tcsetattr(0, TCSANOW, &current);
-    ssize_t count = read(0, &result, 2);
+    ssize_t count = read(0, &result, 1);
     if (count == 0 || count == -1) { result = EOF; }
     tcsetattr(0, TCSANOW, &captured);
     return result;
